cb4b03d6883e9fc2aeea44ccc7eab9558859dcb7
Joziane Oliveira <jozioliveira@edu.uniube.br>
2024-11-12 11:50:37 -0300

    Adiciona verificação de erro na conexão com o banco de dados
    - Adiciona verificação de erro na conexão com o banco de dados `bd_games`.
    - Se a conexão falhar, exibe uma mensagem de erro com o código e a descrição do problema.
    - A execução do script é interrompida usando `die()` em caso de erro na conexão.

--- includes/banco.php
+++ includes/banco.php
@@ -1,3 +1,7 @@
 <?php 
 $banco = new mysqli("localhost", "root", "1234", "bd_games");
+if ($banco->connect_errno) {
+    echo "<p>Foi encontrado um erro $banco->errno --> $banco->connect_error</p>";
+    die();
+} 
 ?>
