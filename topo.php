f9e3a3989ef00afc0923275b8a62a2afd1f0b515
Joziane Oliveira <jozioliveira@edu.uniube.br>
2024-11-30 21:18:58 -0300

    Adição de controle de permissões por tipo de usuário e melhorias no sistema
    - **Funções de Controle de Permissões:**
      - `is_logado`: Verifica se o usuário está logado.
      - `is_admin`: Verifica se o usuário logado possui o tipo de permissão `admin`.
      - `is_editor`: Verifica se o usuário logado possui o tipo de permissão `editor`.
    
    - **Ajustes de Exibição Baseados em Permissões:**
      - Exibição de ações específicas no cabeçalho:
        - Admins podem acessar opções para criar novos usuários e jogos.
        - Link "Meus dados" visível para todos os usuários logados.
      - Exibição de botões de ações (`add_circle`, `edit`, `delete`):
        - Apenas admins veem todas as opções.
        - Editores podem acessar apenas o botão `edit`.
    
    - **Melhorias no Código:**
      - Funções centralizadas para evitar repetição na verificação de permissões.
      - Utilização de ícones da biblioteca Material Symbols para ações administrativas.
      - Validação robusta de sessões para garantir segurança e funcionalidade.
    
    - **Refinamentos na Interface:**
      - Ícones organizados para uma melhor usabilidade e clareza visual.
      - Diferenciação clara entre as permissões e o que cada tipo de usuário pode realizar.
    
    - **Integração com o Banco de Dados:**
      - Ações de admin e editor condicionadas à verificação do tipo de usuário armazenado na sessão.
    
    Essas alterações melhoram a segurança e a personalização da aplicação, oferecendo experiências ajustadas para diferentes níveis de usuários.

--- topo.php
+++ topo.php
@@ -4,6 +4,11 @@ if (empty($_SESSION['user'])) {
     echo "<a href= 'user-login.php'>Entrar</a>";
 } else {
     echo "Olá, <strong>" . $_SESSION['nome'] . "</strong> | ";
+    echo "Meus dados | ";
+    if (is_admin()) {
+        echo "Novo usuário | ";
+        echo "Novo jogo | ";
+    }
     echo "<a href='user-logout.php'>Sair</a>";
     
 }
